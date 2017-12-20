457eb0f4579ee579f7ceeb33139f783de8ed79ca
marco.peinado <mpeinado7@gmail.com>
2017-12-19 22:08:19 -0700

    Script to get data for some high school basketball teams

---
+++ gameData.py
@@ -0,0 +1,90 @@
+# Request module to collect a web page
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# url of page we will scrape
+basketballSchools = [
+    {
+        'school': 'Denver East',
+        'url':'http://www.maxpreps.com/high-schools/denver-east-angels-(denver,co)/basketball/schedule.htm'
+    },
+    {
+        'school': 'Broomfield',
+        'url':'http://www.maxpreps.com/high-schools/broomfield-eagles-(broomfield,co)/basketball/schedule.htm'
+    },
+    {
+        'school': 'Holy Family',
+        'url':'http://www.maxpreps.com/high-schools/holy-family-tigers-(broomfield,co)/basketball/schedule.htm'
+    },
+    {
+        'school': 'George Washington',
+        'url':'http://www.maxpreps.com/high-schools/george-washington-patriots-(denver,co)/basketball/schedule.htm'
+    },
+    {
+        'school': 'Rock Canyon',
+        'url':'http://www.maxpreps.com/high-schools/rock-canyon-jaguars-(highlands-ranch,co)/basketball/schedule.htm'
+    },
+    {
+        'school': 'Smoky Hill',
+        'url':'http://www.maxpreps.com/high-schools/smoky-hill-buffaloes-(aurora,co)/basketball/schedule.htm'
+    }
+]
+
+# Create a file to write to, add headers row
+f = csv.writer(open('games.csv', 'w'))
+
+def getTagContents(tag) :
+    if tag is not None :
+        if len(tag) > 0 :
+            #print(tag.contents[0])
+            return tag.contents[0]
+
+
+def extractGameData(game):
+    #print(game.find(class_='event-date'))
+
+    # Game Date 
+    gameDateTag = game.find(class_='event-date')
+    gameDate = getTagContents(gameDateTag)
+
+    # game time
+    gameTimeTag = game.find(class_='event-time')
+    gameTime = getTagContents(gameTimeTag)
+
+    # opponent
+    opponentTag = game.find(class_='contest-type-indicator')
+    opponent = getTagContents(opponentTag)
+
+
+    # contest-location
+    gameLocation = game.find(class_='contest-location')
+    location = getTagContents(gameLocation)
+
+    # score
+    gameScore = game.find(class_='score')
+    score = getTagContents(gameScore)
+
+    f.writerow([gameDate, gameTime, opponent, location, score])
+    ##print("\n")
+   
+for school in basketballSchools : 
+    f.writerow([school['school']])
+    f.writerow(['date', 'time', 'oponent', 'location', 'score'])
+
+    # asssign the result of a request 
+    page = requests.get(school['url'])
+    print(page.status_code)
+
+    # Create a BeautifulSoup object, or parse tree
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    # Pulling Text from a web page
+    sheduleContainer = soup.find("table", {"id": "schedule"})
+
+    gameList = sheduleContainer.find_all(class_='dual-contest')
+
+    for game in gameList :
+        extractGameData(game)
+
+    f.writerow([''])
---
+++ readme.md
@@ -0,0 +1,23 @@
+
+https://www.digitalocean.com/community/tutorials/how-to-work-with-web-data-using-requests-and-beautiful-soup-with-python-3
+
+. bin/activate
+deactivate
+
+# Tutorials 
+https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
+
+@LEFT Removing Superfluous Data
+
+
+# Intro Tutorial w/ good explanations
+https://www.digitalocean.com/community/tutorials/how-to-work-with-web-data-using-requests-and-beautiful-soup-with-python-3
+
+
+# Installing two libraries that we need
+pip install requests
+pip install beautifulsoup4
+
+
+# How to setup the environment 
+https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-local-programming-environment-on-ubuntu-16-04
